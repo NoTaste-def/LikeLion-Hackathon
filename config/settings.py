a39e7648b882bed64e6590f94289ed03eb90064c
NoTaste-def <ddrnjs1245@gmail.com>
2024-08-02 02:57:32 +0900

    CORS error struggle17

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -146,8 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',  # 세션 인증
-        # 'rest_framework.authentication.BasicAuthentication',   # 기본 인증
-        # 'rest_framework.authentication.TokenAuthentication',  # 토큰 인증
+        'rest_framework.authentication.BasicAuthentication',   # 기본 인증
+        'rest_framework.authentication.TokenAuthentication',  # 토큰 인증
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',  # 인증된 사용자만 접근
