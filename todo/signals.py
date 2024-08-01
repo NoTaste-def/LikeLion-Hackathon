33427c54bc1320e252e3d18f78330996c9ce83cd
NoTaste-def <ddrnjs1245@gmail.com>
2024-08-02 00:25:28 +0900

    CORS error struggle4

--- todo/signals.py
+++ todo/signals.py
@@ -7,4 +7,7 @@ def create_initial_todo_items(sender, **kwargs):
     if sender.name == 'todo':
         items = TodoItem.ITEM_CHOICES
         for item in items:
-            TodoItem.objects.get_or_create(name=item[0], defaults={'name': item[1]})
+            try:
+                TodoItem.objects.get_or_create(name=item[0], defaults={'name': item[1]})
+            except Exception as e:
+                print(f"Error creating item: {item}, Error: {e}")
